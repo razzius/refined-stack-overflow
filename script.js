a96ca1e2abf6fa6736a70f3196b896e25ad92d99
Razzi Abuissa <razzi@abuissa.net>
2021-11-14 22:57:52 -0600

    Remove console.log and reformat

--- script.js
+++ script.js
@@ -1,85 +1,82 @@
-const $myProfile = document.querySelector('.my-profile');
-const {href: myHref} = $myProfile;
+const $myProfile = document.querySelector('.my-profile')
+const { href: myHref } = $myProfile
 
-function isOwnAnswer($answerNode) {
-	const $answerUserLink = $answerNode.querySelector('.user-details a');
-	return $answerUserLink?.href === myHref;
+function isOwnAnswer ($answerNode) {
+  const $answerUserLink = $answerNode.querySelector('.user-details a')
+  return $answerUserLink?.href === myHref
 }
 
-function isNodeUpvoted($answerNode) {
-	return Boolean($answerNode.querySelector('.fc-theme-primary'));
+function isNodeUpvoted ($answerNode) {
+  return Boolean($answerNode.querySelector('.fc-theme-primary'))
 }
 
-function sortAnswerNodes(nodes) {
-	return [...nodes].sort((a, b) => {
-		if (isOwnAnswer(a)) {
-			return -1;
-		}
+function sortAnswerNodes (nodes) {
+  return [...nodes].sort((a, b) => {
+    if (isOwnAnswer(a)) {
+      return -1
+    }
 
-		if (isOwnAnswer(b)) {
-			return 1;
-		}
+    if (isOwnAnswer(b)) {
+      return 1
+    }
 
-		if (isNodeUpvoted(a)) {
-			return -1;
-		}
+    if (isNodeUpvoted(a)) {
+      return -1
+    }
 
-		if (isNodeUpvoted(b)) {
-			return 1;
-		}
+    if (isNodeUpvoted(b)) {
+      return 1
+    }
 
-		return 0;
-	});
+    return 0
+  })
 }
 
-function main() {
-	sortMyAnswersAndUpvotesToTop();
-	highlightMyAnswers();
-	highlightMyUpvotes();
+function main () {
+  sortMyAnswersAndUpvotesToTop()
+  highlightMyAnswers()
+  highlightMyUpvotes()
 }
 
-function highlightMyAnswers() {
-	const $answersContainer = document.querySelector('#answers');
-	const $answerNodes = $answersContainer.querySelectorAll('.answer');
-	for (const $answer of $answerNodes) {
-		if (isOwnAnswer($answer)) {
-			$answer.style.border = '2px solid #44d';
-			document.body.classList.add('my-answer');
-                  $answer.scrollIntoView(false)
-		}
-	}
-}
-function highlightMyUpvotes() {
-	const $answersContainer = document.querySelector('#answers');
-	const $answerNodes = $answersContainer.querySelectorAll('.answer');
-	for (const $answer of $answerNodes) {
-		if (isNodeUpvoted($answer)) {
-			$answer.style.border = '2px solid #44d';
-			document.body.classList.add('my-answer');
-                  $answer.scrollIntoView(false)
-		}
-	}
+function highlightMyAnswers () {
+  const $answersContainer = document.querySelector('#answers')
+  const $answerNodes = $answersContainer.querySelectorAll('.answer')
+  for (const $answer of $answerNodes) {
+    if (isOwnAnswer($answer)) {
+      $answer.style.border = '2px solid #44d'
+      document.body.classList.add('my-answer')
+      $answer.scrollIntoView(false)
+    }
+  }
 }
 
-
-function sortMyAnswersAndUpvotesToTop() {
-	const $answersContainer = document.querySelector('#answers');
-	const $answerNodes = $answersContainer.querySelectorAll('.answer');
-	const $elementBeforeAnswers = $answerNodes[0].previousElementSibling;
-
-
-	const $sortedAnswers = sortAnswerNodes($answerNodes);
-	const chars = [...$sortedAnswers].map(node => ({nodeUrl: node.querySelectorAll('.user-details a'), o: isOwnAnswer(node), u: isNodeUpvoted(node)}));
-	console.log(chars);
-	const $newAnswersFragment = document.createDocumentFragment();
-
-	for (const $answer of $answerNodes) {
-		$answer.remove();
-	}
-
-	$newAnswersFragment.append(...$sortedAnswers);
-
-	$answersContainer.insertBefore($newAnswersFragment, $elementBeforeAnswers.nextSibling);
+function highlightMyUpvotes () {
+  const $answersContainer = document.querySelector('#answers')
+  const $answerNodes = $answersContainer.querySelectorAll('.answer')
+  for (const $answer of $answerNodes) {
+    if (isNodeUpvoted($answer)) {
+      $answer.style.border = '2px solid #44d'
+      document.body.classList.add('my-answer')
+      $answer.scrollIntoView(false)
+    }
+  }
 }
 
-main();
+function sortMyAnswersAndUpvotesToTop () {
+  const $answersContainer = document.querySelector('#answers')
+  const $answerNodes = $answersContainer.querySelectorAll('.answer')
+  const $elementBeforeAnswers = $answerNodes[0].previousElementSibling
+
+  const $sortedAnswers = sortAnswerNodes($answerNodes)
+  const $newAnswersFragment = document.createDocumentFragment()
+
+  for (const $answer of $answerNodes) {
+    $answer.remove()
+  }
+
+  $newAnswersFragment.append(...$sortedAnswers)
+
+  $answersContainer.insertBefore($newAnswersFragment, $elementBeforeAnswers.nextSibling)
+}
+
+main()
